03db61af6b32d79abed39f1ce93f6ac062bfba69
Simon Rabuogi <symokevo@gmail.com>
2023-11-18 10:53:55 +0300

    create assets/forms.css and style the forms appropriately

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
   declarations: [
     AppComponent,
     HeroFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -18,25 +18,7 @@ export class HeroFormComponent {
 
   onSubmit() { this.submitted = true; }
 
-
   newHero() {
     this.model = new Hero(42, '', '');
   }
-
-  skyDog(): Hero {
-    const myHero =  new Hero(42, 'SkyDog',
-                           'Fetch any object at any distance',
-                           'Leslie Rollover');
-    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
-    return myHero;
-  }
-
-  // Reveal in html:
-  //   Name via form.controls = {{showFormControls(heroForm)}}
-  showFormControls(form: any) {
-    return form && form.controls.name &&
-    form.controls.name.value; // Dr. IQ
-  }
-
 }
-
